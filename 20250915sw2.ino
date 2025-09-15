6dc9092d131fcb56c433a40c7bdc392a8a363cfe
rladuddn43 <Harold303363@gmail.com>
2025-09-15 13:05:45 +0900

    Add files via upload

---
+++ 20250915sw2.ino
@@ -0,0 +1,35 @@
+#define PIN_LED 7
+unsigned count,toggle;
+
+void setup() {
+  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(115200);
+  while (!Serial){
+    ;}
+   Serial.println("Hello World!");
+   count = toggle = 0;
+   digitalWrite(PIN_LED,toggle); // 처음 LED ON
+   delay(1000);
+   toggle = 1;
+   digitalWrite(PIN_LED,toggle); //1초뒤 LED OFF
+}
+
+void loop() {
+  for (count;count <= 10;count++){
+    delay(100);
+    digitalWrite(PIN_LED,toggle);
+      
+    if (toggle == 0){
+      toggle =1;
+      }
+    else {
+      toggle = 0;
+      }       
+    }
+    toggle ==1;
+   while (true){
+    ;
+   }
+
+  
+}
